85691f250a2757764e5564042924259b3920a398
zhiwen <zhiwen@kooboo.cn>
2019-08-28 19:54:43 +0800

    add controller

---
+++ controllers/homeController.go
@@ -0,0 +1,5 @@
+package controllers
+
+func Hello() string{
+	return "hello golang!"
+}
--- go-deployment.go
+++ go-deployment.go
@@ -1,12 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zhiwen-kooboo/go-deployment/controllers"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": controllers.Hello(),
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
